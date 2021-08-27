dbddd054e5ad0efd72022c636933d586688d1dd0
YeongSeok Kim <youngstone89@icloud.com>
2021-08-27 15:13:47 +0900

    vendor created

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-
-		c.JSON(200, gin.H{
-
-			"message": "hello world",
-		})
-
-	})
+	router.GET("/:name", IndexHandler)
 
 	router.Run(":5000")
 
+}
+func IndexHandler(c *gin.Context) {
+
+	// name := c.Params.ByName("name")
+
+	// c.JSON(200, gin.H{
+	// 	"message": "hello " + name,
+	// })
+
+	c.XML(200, Person{FirstName: "YeongSeok", LastName: "Kim"})
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:person`
+	FirstName string   `xml:firstName,attr`
+	LastName  string   `xml:lastName,attr`
 }
